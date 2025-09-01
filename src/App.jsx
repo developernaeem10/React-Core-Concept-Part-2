e51d8986906ffa0ef4437073025393a9fb451eda
developernaeem10 <developer.naeem96@gmail.com>
2025-09-01 20:30:49 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,62 @@
-import './App.css'
-import Counter from './counter'
-import Batsman from './Batsman'
+import "./App.css";
+import Counter from "./counter";
+import Batsman from "./Batsman";
+import Users from "./users";
+import Friends from "./Friends";
+import { Suspense } from "react";
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+);
+
+const fetchFriends = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  return res.json()
+}
 
 function App() {
-  
 
-  function handleClick () {
-    alert("Clicked")
+  const friendsPromise = fetchFriends()
+  function handleClick() {
+    alert("Clicked");
   }
   const newclick = () => {
-    alert("Hello Click 3")
-  }
-
+    alert("Hello Click 3");
+  };
 
   const addNewNum = (num) => {
-    const add = num + 10
-    alert(add)
-  } 
+    const add = num + 10;
+    alert(add);
+  };
 
   return (
     <>
       <h3>Vite + React</h3>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Users fetchUsers = {fetchUsers}></Users>
+      </Suspense>
+
+      <Suspense fallback={<p>Friends are Loading...</p>}>
+        <Friends friendsPromise = {friendsPromise}></Friends>
+      </Suspense>
+
       <Batsman></Batsman>
       <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
-      <button onClick={function justClick (){
-        alert("Clicked 2")
-      }}>Click Me 2</button>
-    <button onClick={newclick}>Click Me 3</button>
-    <button onClick={
-      () => alert("Click Me 4")
-    }>Click Me 4</button>
+      <button
+        onClick={function justClick() {
+          alert("Clicked 2");
+        }}
+      >
+        Click Me 2
+      </button>
+      <button onClick={newclick}>Click Me 3</button>
+      <button onClick={() => alert("Click Me 4")}>Click Me 4</button>
 
-    <button onClick={()=> addNewNum(25)}>Click Me 5</button>
-
+      <button onClick={() => addNewNum(25)}>Click Me 5</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Batsman.jsx
+++ src/Batsman.jsx
@@ -1,46 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Batsman () {
+export default function Batsman() {
+  const [runs, setRuns] = useState(0);
 
-    const [runs, setRuns] = useState(0)
+  const [sixes, setSixes] = useState(0);
 
-    const [sixes, setSixes] = useState(0)
+  const [bowls, setBowls] = useState(0);
 
-    const [bowls, setBowls] = useState(0)
+  const handleSingles = () => {
+    const updateRuns = runs + 1;
+    setRuns(updateRuns);
+  };
 
-    const handleSingles = () => {
-        const updateRuns = runs + 1
-        setRuns(updateRuns)
-    }
+  const handleSixes = () => {
+    const updateSixes = runs + 6;
+    const newsixes = sixes + 1;
+    setSixes(newsixes);
+    setRuns(updateSixes);
+  };
 
-    const handleSixes = () => {
-        const updateSixes = runs + 6
-        const newsixes = sixes + 1
-        setSixes(newsixes)
-        setRuns(updateSixes)
-    }
+  const handleBowlers = () => {
+    const newBowlers = bowls + 1;
+    setBowls(newBowlers);
+  };
 
-    const handleBowlers = () => {
-        const newBowlers = bowls + 1
-        setBowls(newBowlers)
-    }
-
-    return (
-        <div>
-            <h3>Player : Bangla Batsman</h3>
-            <p><small>Six : {sixes}</small></p>
-            {
-                runs > 50 && <p>Congratulates for 50</p>
-            }
-            {
-                bowls > 6 && <p>New Over</p>
-            }
-            <h1>Score : {runs}</h1>
-            <h1>Bowls : {bowls}</h1>
-            <button onClick={handleSingles}>Singles</button>
-            <button onClick={handleBowlers}>Bowler</button>
-            <button>Fours</button>
-            <button onClick={handleSixes}>Sixes</button>
-        </div>
-    )
+  return (
+    <div>
+      <h3>Player : Bangla Batsman</h3>
+      <p>
+        <small>Six : {sixes}</small>
+      </p>
+      {runs > 50 && <p>Congratulates for 50</p>}
+      {bowls > 6 && <p>New Over</p>}
+      <h1>Score : {runs}</h1>
+      <h1>Bowls : {bowls}</h1>
+      <button onClick={handleSingles}>Singles</button>
+      <button onClick={handleBowlers}>Bowler</button>
+      <button>Fours</button>
+      <button onClick={handleSixes}>Sixes</button>
+    </div>
+  );
 }
